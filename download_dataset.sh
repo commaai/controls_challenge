79cd02b6b8af2f75fae46b8d6b1127385b343eda
Yaseen <9275716+ynx0@users.noreply.github.com>
2024-05-18 16:22:38 -0500

    Update download_dataset.sh to use `curl`
    makes it work on mac where wget isn't available by default

--- download_dataset.sh
+++ download_dataset.sh
@@ -9,9 +9,10 @@ cd data
 URL="https://huggingface.co/datasets/commaai/commaSteeringControl/resolve/main/data/SYNTHETIC_V0.zip"
 
 echo "Downloading dataset from $URL"
-wget "$URL"
 
-ZIP_FILE="${URL##*/}"
+ZIP_FILE="dataset.zip"
+
+curl -L "$URL" -o $ZIP_FILE
 
 echo "Unzipping $ZIP_FILE"
 unzip -jq "$ZIP_FILE"
